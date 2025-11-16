a105f721607519b7f2cbb9b2b95661351046f485
Taiga Chang <tchang3@pride.hofstra.edu>
2025-11-15 21:48:33 -1000

    Adding routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
 
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Homepage from './pages/home-page.jsx';
-
-
+import AuthPage from './pages/auth-page.jsx';
+import HowItWorks from './pages/how-it-works.jsx';
+import FAQ from './pages/faq.jsx';
 
 function App() {
   return (
     <>
       <Header />
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/how-it-works" element={<HowItWorks />} />
+        <Route path="/faq" element={<FAQ />} />
+      </Routes>
     </>
   );
 }
 
-export default App
+export default App;
